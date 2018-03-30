22f671a92fe3e097544177e7ca119d104f6322cb
lukew <luke.waninger@gmail.com>
2018-03-29 23:11:11 -0700

    .

--- hmwk1.py
+++ hmwk1.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import scipy as sp
 
 # (a)
 college = pd.read_csv("College.csv")
@@ -8,4 +10,11 @@ college.head(5)
 college.rename(columns={'Unnamed: 0': 'School'}, inplace=True)
 college.set_index('School')
 
-# (c)
+# (c) i.
+college.describe()
+
+# (c) ii.
+pd.scatter_matrix(college.iloc[:, [3,5]])
+
+# (c) iii. TODO: FINISH
+college.loc[:, ['Room.Board', 'Private']].boxplot()
