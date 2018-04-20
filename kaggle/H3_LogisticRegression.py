1ce563474eb7a6d0fe4bb26412fadcc9008e2eab
lukew <luke.waninger@gmail.com>
2018-04-20 11:55:30 -0700

    .

---
+++ kaggle/H3_LogisticRegression.py
@@ -0,0 +1,188 @@
+from multiprocessing import Lock
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+import os
+
+# misc setup for readability
+norm = np.linalg.norm
+exp = np.exp
+log = np.log
+
+
+class MyLogisticRegression:
+    def __init__(self, X_train, y_train, lamda=2, max_iter=500, eps=0.001, idx=0):
+        self._betas = None
+        self._eps = eps
+        self._lamda = lamda
+        self._max_iter = max_iter
+
+        self.__idx = idx
+        self.__x = X_train
+        self.__y = y_train
+        self.__n, self.__d = X_train.shape
+
+        self.__eta = 1 # self.__calc_t_init()i
+        self.__objective_vals = None
+        self.__training_errors = None
+        self.__thetas = None
+
+    @property
+    def coef_(self):
+        return self._betas[-1].reshape(1, 57) if self._betas is not None else []
+
+    @property
+    def objective_vals_(self):
+        if self.__objective_vals is not None:
+            return self.__objective_vals
+        else:
+            self.__objective_vals = [self.__objective(b) for b in self._betas] if self._betas is not None else []
+            return self.__objective_vals
+
+    @property
+    def training_errors_(self):
+        if self.__training_errors is not None:
+            return self.__training_errors
+        else:
+            self.__training_errors = []
+
+            for b in self._betas:
+                pre = self.predict(self.__x, b)
+                acc = np.sum([1 if yh == yt else 0 for yh, yt in zip(pre, self.__y)]) / self.__n
+                self.__training_errors.append(1 - acc)
+        return self.__training_errors
+
+    # public methods
+    def fit(self, algo='grad', init_method='zeros'):
+        def init(method):
+            if method == 'ones':
+                b = [np.ones(self.__d)]
+            elif method == 'zeros':
+                b = [np.zeros(self.__d)]
+            elif method == 'normal':
+                b = [np.random.normal(0, 1, self.__d)]
+            else:
+                raise Exception('init method not defined')
+            return b
+
+        self._betas = init(init_method)
+        self.__objective_vals = None
+        self.__training_errors = None
+
+        if algo == 'grad':
+            self.__graddescent()
+        elif algo == 'fgrad':
+            self._betas.append(self._betas[-1])
+            self.__thetas = init(init_method)[0]
+            self.__fastgradalgo()
+            self._betas = self._betas[1:]
+        else:
+            raise Exception("algorithm <%s> is not available" % algo)
+
+        self._betas = self._betas[1:]
+        return self
+
+    def objective(self, coef):
+        return self.__objective(coef)
+
+    def predict(self, x, betas=None):
+        if betas is not None:
+            b = betas
+        else:
+            b = self.coef_
+
+        return [1 if xi @ b > 0 else -1 for xi in x]
+
+    def predict_proba(self, x, betas=None):
+        if betas is not None:
+            b = betas
+        else:
+            b = self.coef_
+
+        return [exp(xi@b)/(1 + exp(xi@b)) for xi in x]
+
+    # private methods
+    def __backtracking(self, beta, t_eta=0.5, alpha=0.5):
+        l, t = self._lamda, self.__eta
+
+        gb = self.__computegrad(beta)
+        n_gb = norm(gb)
+
+        found_t, i = False, 0
+        while not found_t and i < self._max_iter:
+            if self.__objective(beta - t*gb) < self.__objective(beta) - alpha * t * n_gb**2:
+                found_t = True
+            elif i == self._max_iter-1:
+                break
+            else:
+                t *= t_eta
+                i += 1
+
+        self.__eta = t
+        return self.__eta
+
+    def __calc_t_init(self):
+        x, l, n = self.__x, self._lamda, self.__n
+
+        m = np.max(1/n * np.linalg.eigvals(x.T @ x)) + l
+        return 1 / np.float(m)
+
+    def __computegrad(self, b):
+        x, y, l, n = self.__x, self.__y, self._lamda, self.__n
+
+        p = (1 + exp(y * (x @ b))) ** -1
+        return 2 * l * b - (x.T @ np.diag(p) @ y) / n
+
+    def __graddescent(self):
+        grad_x = self.__computegrad(self._betas[-1])
+
+        i = 0
+        while norm(grad_x) > self._eps and i < self._max_iter:
+            b0 = self._betas[-1]
+            t = self.__backtracking(b0)
+
+            self._betas.append(b0 - t * grad_x)
+            grad_x = self.__computegrad(b0)
+
+            i += 1
+
+    def __fastgradalgo(self):
+        theta = self.__thetas
+        grad = self.__computegrad(theta)
+
+        i = 0
+        while norm(grad) > self._eps and i < self._max_iter:
+            b0 = self._betas[-1]
+            t  = self.__backtracking(b0)
+            grad = self.__computegrad(theta)
+
+            b1 = theta - t*grad
+            self._betas.append(b1)
+
+            theta = b1 + (i/(i+3))*(b1-b0)
+            i += 1
+
+            self.__print_status("%s, %s, %s, %s, %s, %s, %s\n" %
+                                (self.__idx, os.getpid(),
+                                 i, t, norm(grad),
+                                 self.__objective(b0),
+                                 norm(self._betas[-1])))
+
+    def __print_status(self, message):
+        print(message)
+        with open("/mnt/hgfs/descent_logs/descent_log_%s.csv" % self.__idx, "a+") as f:
+            f.writelines(message)
+
+    def __objective(self, beta):
+        x, y, n, l = self.__x, self.__y, self.__n, self._lamda
+
+        return np.sum([log(1 + exp(-yi * xi.T @ beta)) for xi, yi in zip(x, y)]) / n + l * norm(beta) ** 2
+
+    def __repr__(self):
+        return "MyLogisticRegression(C=%s, eps=%s, max_iter=%s)" % (self._lamda, self._eps, self._max_iter)
+
--- kaggle/image_net_ovr.py
+++ kaggle/image_net_ovr.py
@@ -1,19 +1,21 @@
-from homework.H3_LogisticRegression import MyLogisticRegression
+from H3_LogisticRegression import MyLogisticRegression
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
-from multiprocessing import Process
+from multiprocessing import Process, Queue
 import numpy as np
 import os
 import pandas as pd
 import pickle
 import statsmodels.api as sm
+import time
 
 
 def datapath(filename):
-    return os.getcwd() + '\\kaggle\\data\\' + filename
+    return os.getcwd() + '/data/' + filename
+
 
 x_train = np.load(datapath('train_features.npy'))
 y_train = np.load(datapath('train_labels.npy'))
@@ -24,50 +26,37 @@ scalar = MinMaxScaler().fit(x_train)
 x_train = scalar.transform(x_train)
 x_val = scalar.transform(x_val)
 
-# train using a one vs. all approach
-y_train_sets, cvs = [], []
-# try:
-#     data = None
-#     with open(datapath('classifiers.pickle'), 'rb') as f:
-#         data = pickle.load(f)
-#
-#     y_train_sets = data['training_sets']
-#     cvs = data['classifiers']
-# except FileNotFoundError:
 
-
-def train_one(idx):
+def train_one(idx, log_queue):
     # set class labels
     y = np.copy(y_train)
     y[y != idx] = -1
     y[y == idx] =  1
 
     # train this class vs the rest
-    print("fitting %s vs rest on pid %s" % (i, os.getpid()))
-    cv = MyLogisticRegression(X_train=x_train, y_train=y, lamda=.1, eps=0.0001)
-    cv = cv.fit(algo='fgrad', init_method='normal')
-    cvs.append(cv)
+    print("fitting %s vs rest on pid %s" % (idx, os.getpid()))
+    cv = MyLogisticRegression(X_train=x_train, y_train=y, lamda=.05, eps=0.001, idx=idx, log_queue=log_queue)
+    cv = cv.fit(algo='fgrad', init_method='zeros')
 
-    # pickle them out so I don't have to wait again
-    to_dump = {
-        'training_sets': y_train_sets,
-        'classifiers': cvs
-    }
+    # pickle them out
     with open(datapath('classifier_%svr.pickle' % idx), 'wb') as f:
-        pickle.dump(to_dump, f, pickle.HIGHEST_PROTOCOL)
+        pickle.dump(cv, f, pickle.HIGHEST_PROTOCOL)
+
+    print("%s finished %s vs rest" % (os.getpid(), idx))
 
 
-[Process(target=train_one, args=(i,)).start() for i in np.unique(y_train)]
+qu = Queue()
+processes = [Process(target=train_one, args=(i,qu)) for i in np.unique(y_train)]
 
-# make predictions (train/val) and plot per iteration
-ppi = []
-for i in range(len(cvs[0]._betas)):
-    predictions = np.array([cv.predict_proba(x_train, cv._betas[i]) for cv in cvs])
-    ppi.append(np.argmax(predictions, axis=0))
+for process in processes:
+    time.sleep(4)
+    process.start()
 
-err = []
-for i in range(len(ppi)):
-    acc = np.sum([1 if yh == yt else 0 for yh, yt in zip(ppi[i], y_train)])/5000
-    err.append(1-acc)
+while True:
+    m = qu.get()
 
-
+    if m.message == 'END_FLAG':
+        children -= 1
+        if children == 0: break
+ 
+    
