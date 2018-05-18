aad82f0eae4f795755f7b7a72b423fbd4495d482
lukeWaninger <luke.waninger@gmail.com>
2018-05-17 19:24:27 -0700

    .

--- homework/H6_SmoothedHingeSVM.py
+++ homework/H6_SmoothedHingeSVM.py
@@ -207,13 +207,13 @@ def ex1():
                          log_path='.', logging_level='reduced').fit()
 
     predictions = cv.predict(x_val)
-    error = np.mean(predictions == y_val)
+    error = 1-np.mean(predictions == y_val)
 
     with open('ex1.txt', 'a+') as f:
         f.write(f'cv: {str(error)}\n')
 
 
-def ex2():
+def ex2b_ap():
     x_train = np.load('./data/h6/train_features.npy')
     y_train = np.load('./data/h6/train_labels.npy')
     x_val   = np.load('./data/h6/val_features.npy')
@@ -221,17 +221,20 @@ def ex2():
     x_test  = np.load('./data/h6/test_features.npy')
 
     # select a small subset of classes for proof of concept
-    test_classes = [0, 1, 2]
-    idx = np.isin(y_train, test_classes)
+    # test_classes = [0, 1, 2]
+    # idx = np.isin(y_train, test_classes)
+    #
+    # x_train = x_train[idx, :]
+    # y_train = y_train[idx]
+    #
+    # idx   = np.isin(y_val, test_classes)
+    # x_val = x_val[idx, :]
+    # y_val = y_val[idx]
 
-    x_train = x_train[idx, :]
-    y_train = y_train[idx]
-
-    idx   = np.isin(y_val, test_classes)
-    x_val = x_val[idx, :]
-    y_val = y_val[idx]
-
-    ex2a_parameters = {
+    # --------------------------------------------------
+    # exercise 2b. all pairs, 4 fold cross val
+    # --------------------------------------------------
+    ex2b_ap_params = {
         'classifiers': [
             {
                 'type': 'linear_svm',
@@ -243,29 +246,71 @@ def ex2():
                     'bt_max_iter': [50],
                     'eps': [.001],
                     'eta': [1.],
-                    'lambda': [1.],
-                    'max_iter': [2],
-                    't_eta': [0.8]
+                    'lambda': [.25, .5, 1., 2.],
+                    'max_iter': [100],
+                    't_eta': [0.8],
                 }
             }
         ]
     }
 
-    task = 'ex2a'
-    cv = MultiClassifier(x_train=x_train, y_train=y_train, parameters=ex2a_parameters,
-                         x_val=x_val, y_val=y_val, n_jobs=3,
-                         classification_method='ovr', task=task,
+    task = 'ex2b_ap'
+    cv = MultiClassifier(x_train=x_train, y_train=y_train, parameters=ex2b_ap_params,
+                         x_val=x_val, y_val=y_val, n_jobs=-1,
+                         classification_method='all_pairs', task=task,
                          log_path='.', logging_level='reduced').fit()
-
     cv.output_predictions(x_test)
 
     predictions = cv.predict(x_val)
-    error = np.mean(predictions == y_val)
+    error = 1-np.mean(predictions == y_val)
+    with open('ex2.txt', 'a+') as f:
+        f.write(f'{task}: {str(error)}\n')
 
+
+def ex2b_ovr():
+    # --------------------------------------------------
+    # exercise 2b. one vs. rest, 4 fold cross val
+    # --------------------------------------------------
+    x_train = np.load('./data/h6/train_features.npy')
+    y_train = np.load('./data/h6/train_labels.npy')
+    x_val = np.load('./data/h6/val_features.npy')
+    y_val = np.load('./data/h6/val_labels.npy')
+    x_test = np.load('./data/h6/test_features.npy')
+
+    ex2b_ovr_params = {
+        'classifiers': [
+            {
+                'type': 'linear_svm',
+                'parameters': {
+                    'loss': ['smoothed_hinge'],
+                    'h': [0.5],
+                    'algo': ['fgrad'],
+                    'alpha': [0.5],
+                    'bt_max_iter': [50],
+                    'eps': [.001],
+                    'eta': [1.],
+                    'lambda': [.25, .5, 1., 2.],
+                    'max_iter': [100],
+                    't_eta': [0.8],
+                }
+            }
+        ]
+    }
+
+    task = 'ex2b_ovr'
+    cv = MultiClassifier(x_train=x_train, y_train=y_train, parameters=ex2b_ovr_params,
+                         x_val=x_val, y_val=y_val, n_jobs=3,
+                         classification_method='all_pairs', task=task,
+                         log_path='.', logging_level='reduced').fit()
+    cv.output_predictions(x_test)
+
+    predictions = cv.predict(x_val)
+    error = 1 - np.mean(predictions == y_val)
     with open('ex2.txt', 'a+') as f:
         f.write(f'{task}: {str(error)}\n')
 
 
 if __name__ == '__main__':
     #ex1()
-    ex2()
+    ex2b_ap()
+    #ex2b_ovr()
